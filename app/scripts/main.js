59b08c8414b2c69c6cd2e96ff3148eae6ee2cf44
currensy <abramovisrael@gmail.com>
2016-11-06 21:56:36 +0200

    final design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,24 @@
+'use strict';
 $(document).ready(function(){
+  getQuote();
+  $("#get-random-quote").click(function(){
+    getQuote();
+  });
+  //$("#get-random-quote").click(getQuote());
+});
+
+function getQuote() {
 
   var quotesApi = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
   var proxy = "https://cors-anywhere.herokuapp.com/";
 
+  var quoteIconLeft = '<i class="fa fa-quote-left" aria-hidden="true"></i> ';
+  var quoteIconRight = ' <i class="fa fa-quote-right" aria-hidden="true"></i>';
 
-  $("#get-random-quote").click(function(){
-    var quoteIconLeft = '<i class="fa fa-quote-left" aria-hidden="true"></i> ';
-    var quoteIconRight = ' <i class="fa fa-quote-right" aria-hidden="true"></i>';
-
-    var tweeterBtn = '<a class="twitter-share-button"'
-      +'href="https://twitter.com/intent/tweet?text=Hello%20world">'
-      +'Tweet</a>';
-
-    console.log("Get quote");
-    $.get(proxy+quotesApi,function(data){
-      console.log(data);
-      $("#quote-text").html(quoteIconLeft+ data.quoteText +quoteIconRight);
-      $("#quote-author").html("- " + data.quoteAuthor);
-
-
-      $("#tweet").attr("href","https://twitter.com/intent/tweet?text="+data.quoteText+" -"+data.quoteAuthor);
-
-
-
-    });
-
-
-
-
+  $.get(proxy+quotesApi,function(data){
+    $("#quote-text").html(quoteIconLeft+ data.quoteText +quoteIconRight)
+    $("#quote-author").html("- " + data.quoteAuthor);
+    $("#tweet").attr("href","https://twitter.com/intent/tweet?text="+data.quoteText+" -"+data.quoteAuthor);
+    $(".quote-block").css('visibility','visible').hide().fadeIn("slow");
   });
-
-
-});
+}
